a5e74ed5449df4bee17b13bb7b18969623fb36c4
Kaio <kaiohtf38@gmail.com>
2025-01-15 16:28:52 -0300

    Initial commit

---
+++ src/test/java/br/com/kaioferreira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaioferreira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
